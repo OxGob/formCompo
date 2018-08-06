5e6a264a848b5f8c99a19534912ef7cc643c7a05
oxgob <arvingo@yahoo.co.uk>
2018-08-06 18:07:22 +0100

    Added new starting page and compTester
    To be used as skeleton for new dev

---
+++ focompo/src/pages/startingPage.vue
@@ -0,0 +1,31 @@
+<template>
+  <q-page>
+    <q-card class="bg-cyan-2 q-ma-xl">
+      <q-card-main>
+        <!-- <formBuilder1></formBuilder1> -->
+        <compTest1></compTest1>
+      </q-card-main>
+    </q-card>
+  </q-page>
+</template>
+
+<script>
+import formBuilder from 'components/formBuilder.vue'
+import compTest from 'components/compTester.vue'
+export default {
+  components: {
+    'formBuilder1': formBuilder,
+    'compTest1': compTest
+  },
+  data () {
+    return {
+      forms: [
+        {
+          fDescription: '',
+          fname: ''
+        }
+      ]
+    }
+  }
+}
+</script>
