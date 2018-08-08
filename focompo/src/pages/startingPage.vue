d8ca3c85e7514aeec0abaf3c4e696c36aba4da98
oxgob <arvingo@yahoo.co.uk>
2018-08-08 13:39:46 +0100

    Added more on form tracker

--- focompo/src/pages/startingPage.vue
+++ focompo/src/pages/startingPage.vue
@@ -3,11 +3,14 @@
     <q-card class="bg-cyan-2 q-ma-xl">
       <q-card-main>
         <div v-for="(foG, formInd) in formsg" :key=foG.id>
-          <q-field class="q-mb-sm" label="Form ID: ">
-            <q-input v-model="foG.formID" type="text" align="center" clearable />
+          <div class="q-mb-md q-mt-lg">
+            <q-btn label="Add Form" color = "amber" @click="addFormTapped(formInd)"/>
+            <q-btn class="q-ml-md" v-show="formInd !==0" label="Remove Form" color = "pink" @click="removeForm(formInd)"/>
+          </div>
+          <q-field class="q-mb-sm" label="Form Label: ">
+            <q-input v-model="foG.formLabel" type="text" align="center" clearable />
           </q-field>
-          <q-btn label="Add Form" color = "amber" @click="addFormTapped"/>
-          <q-btn class="q-ml-md" label="Remove Form" color = "pink" @click="removeForm(formInd)"/>
+          <q-card-separator class="q-mb-md q-mt-lg"/>
         </div>
         <!-- <compTestG1 @chiFoTitle="forms.fname = $event"
          @chiFoDescr="forms.fDescription = $event"
@@ -31,35 +34,58 @@ export default {
       counterFormsg: 0,
       formsg: [
         {
-          formID: 0,
+          formLabel: 'form 0',
           indexFo: 0
         }
       ],
-      someT: '',
-      forms:
-        {
-          fDescription: '',
-          fname: ''
-        },
       tesSeveral: [{
-      }]
+      }],
+      formTracker: [
+        {
+          formTkID: 'form 0',
+          indexOfForm: 0
+        }
+      ]
     }
   },
   methods: {
-    addFormTapped () {
+    addFormTapped (formInd) {
       this.counterFormsg++
       this.$q.notify('Form added. new counter is: ' + this.counterFormsg)
-      this.addFormRow()
+      this.addFormRow(formInd)
     },
-    removeForm (index) {
-      this.$q.notify('Remove Form')
-      this.formsg.splice(index, 1)
+    removeForm (formInd) {
+      this.formsg.splice(formInd, 1)
+      this.updtFormTrackerRemove(formInd)
     },
-    addFormRow () {
+    addFormRow (formInd) {
       this.formsg.push({
-        formID: this.counterFormsg,
+        formLabel: 'Form ' + this.counterFormsg,
         indexFo: this.counterFormsg
       })
+      this.updtFormTrackerAdd(formInd)
+    },
+    updtFormTrackerAdd (formInd) {
+      var lastIndexFormObj = this.formsg.length - 1
+      this.formTracker.push({
+        formTkID: this.formsg[lastIndexFormObj].formLabel,
+        indexOfForm: lastIndexFormObj
+      })
+    },
+    updtFormTrackerRemove (formInd) {
+      var foTk = this.formTracker
+      // remove the selected index from the form tracking Array
+      foTk.splice(formInd, 1)
+      // In the tracking array, update the form index for those removed AFTER SPLICE index
+      var lenTrkAfterSplice = foTk.length
+      if (formInd < lenTrkAfterSplice) {
+      // If formInd is NOT LAST, then from formInd position till last, subtract 1 from values of indexOfForm
+        var i
+        for (i = formInd; i < lenTrkAfterSplice; i++) {
+          var fn = foTk[i].indexOfForm - 1
+          foTk[i].indexOfForm = fn
+        }
+      }
     }
   }
 }
