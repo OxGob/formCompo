1961393a0e808b3fcbbc9e0992cbe2e68c0c8bd6
oxgob <arvingo@yahoo.co.uk>
2018-08-08 11:25:01 +0100

    added plus minus row

--- focompo/src/pages/startingPage.vue
+++ focompo/src/pages/startingPage.vue
@@ -1,11 +1,17 @@
 <template>
   <q-page>
-    <q-card class="bg-grey-5 q-ma-xl">
+    <q-card class="bg-cyan-2 q-ma-xl">
       <q-card-main>
-        <q-btn label="Add Form" color = "amber" @click="addFormTapped"/>
-        <compTestG1 @chiFoTitle="forms.fname = $event"
+        <div v-for="(foG, formInd) in formsg" :key=foG.id>
+          <q-field class="q-mb-sm" label="Form ID: ">
+            <q-input v-model="foG.formID" type="text" align="center" clearable />
+          </q-field>
+          <q-btn label="Add Form" color = "amber" @click="addFormTapped"/>
+          <q-btn class="q-ml-md" label="Remove Form" color = "pink" @click="removeForm(formInd)"/>
+        </div>
+        <!-- <compTestG1 @chiFoTitle="forms.fname = $event"
          @chiFoDescr="forms.fDescription = $event"
-         @chiObjFo="tesSeveral = $event"></compTestG1>
+         @chiObjFo="tesSeveral = $event"></compTestG1> -->
         <!-- <compTestG1 :forms.fname /> -->
       </q-card-main>
     </q-card>
@@ -22,6 +28,13 @@ export default {
   },
   data () {
     return {
+      counterFormsg: 0,
+      formsg: [
+        {
+          formID: 0,
+          indexFo: 0
+        }
+      ],
       someT: '',
       forms:
         {
@@ -34,7 +47,19 @@ export default {
   },
   methods: {
     addFormTapped () {
-      this.$q.notify('Form added')
+      this.counterFormsg++
+      this.$q.notify('Form added. new counter is: ' + this.counterFormsg)
+      this.addFormRow()
+    },
+    removeForm (index) {
+      this.$q.notify('Remove Form')
+      this.formsg.splice(index, 1)
+    },
+    addFormRow () {
+      this.formsg.push({
+        formID: this.counterFormsg,
+        indexFo: this.counterFormsg
+      })
     }
   }
 }
