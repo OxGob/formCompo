52f42105d00fed406f8d2a2ed9e6007b3f7a08fd
oxgob <arvingo@yahoo.co.uk>
2018-08-08 18:25:37 +0100

    Com Test Gen 1

--- focompo/src/components/compTesterGen.vue
+++ focompo/src/components/compTesterGen.vue
@@ -1,13 +1,17 @@
 <template>
-    <q-card class="bg-cyan-2 q-ma-xl">
+    <q-card class="bg-white q-ma-xl">
         <q-card-main>
+        <!-- QDes - Forms  -->
              <q-field class="q-mb-sm" label="Form Title Prop: " helper="Please enter the title of the form. This IS displayed to the user.">
-                <q-input v-model="fo.mTitle" type="text" align="center" clearable />
+                <q-input v-model="formG.fname" type="text" align="center" clearable />
             </q-field>
             <q-field class="q-mb-sm" label="Form Description: " helper="Please enter a description for the form. This IS displayed to the user.">
-                <q-input v-model="fo.mDescr" type="textarea" rows="5" align="center" clearable />
-              </q-field>
-            <q-btn class="float-right" label="Save Form / Fire Obj" color="purple" @click="emitToParentObj"/>
+                <q-input v-model="formG.fDescription" type="textarea" rows="5" align="center" clearable />
+            </q-field>
+            <q-card-separator class="q-mb-md q-mt-lg"/>
+        <!-- QDes - Questions -->
+        <!-- QDes - Answers -->
+            <q-btn class="q-ml-lg" label="Save Form / Fire Obj" color="purple" @click="emitToParentObj"/>
         </q-card-main>
     </q-card>
 </template>
@@ -17,12 +21,66 @@ export default {
   props: ['formTitle'],
   data () {
     return {
-      mutableTitle: '',
-      mutDescr: '',
-      fo: {
-        mTitle: '',
-        mDescr: ''
-      }
+      formG:
+        {
+          // ansRadioVal: '',
+          // answers: [
+          //   {
+          //     questionId: '',
+          //     answerText: '',
+          //     answerId: '',
+          //     timeStamp: ''
+          //   }
+          // ],
+          counterGenQuID: 0,
+          // counterAnswers: 0,
+          fDescription: '',
+          fname: '',
+          questions: [
+            {
+              qtext: '',
+              qHelp: '',
+              qId: 'Initial Question',
+              nextDefaultId: '',
+              qType: 'single',
+              answerChoices: [
+                {
+                  answerId: 'Answer 1',
+                  text: '',
+                  nextQuId: '',
+                  showAnswerLabelError: false
+                }
+              ],
+              // ansTrackingID: [
+              //   {
+              //     ansID: 'Answer 1',
+              //     ansIndex: 0
+              //   }
+              // ],
+              counterAnsChID: 0,
+              showNextQIdError: false,
+              showQIdError: false
+            }
+          ],
+          qTrackingID: [
+            {
+              quesID: 'Initial Question',
+              quesIndex: 0
+            }
+          ],
+          quSelOptions: [
+            {
+              label: 'Freetext',
+              value: 'freetext'
+            },
+            {
+              label: 'Single choice',
+              value: 'single'
+            }
+          ],
+          // tempAnsHolder: '',
+          trackQuID: ''
+        }
     }
   },
   methods: {
@@ -36,8 +94,8 @@ export default {
     },
     emitToParentObj () {
       this.$q.notify('emit to Parent as object Fired 2:' + this.fo)
-      console.log('Emit fo  Obj is : ', this.fo)
-      this.$emit('chiObjFo', this.fo)
+      console.log('Emit fo  Obj is : ', this.formG)
+      this.$emit('chiObjFo', this.formG)
     }
   }
 }
--- focompo/src/pages/startingPage.vue
+++ focompo/src/pages/startingPage.vue
@@ -40,8 +40,6 @@ export default {
           indexFo: 0
         }
       ],
-      tesSeveral: [{
-      }],
       formTracker: [
         {
           formTkID: 'form 0',
